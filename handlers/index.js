1599c68372101cd2df77e5976339d3dfd6daebdf
Andrew Dacenko <a.s.dvornik@gmail.com>
2016-04-21 07:00:54 +0300

    add(default handler) logs

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,8 @@
 import {handleMessageMiddleware} from './handle-message';
 
 function defaultHandler(req, res) {
+    console.log('unhandled request for', req.body);
+
     res.sendStatus(404);
 }
 
