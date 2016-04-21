0f53d8ba72397dbdbf074542800d732f0b4cbf5a
Andrew Dacenko <a.s.dvornik@gmail.com>
2016-04-21 06:00:32 +0300

    add(webhooks)

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 /* global process */
 import dotenv from 'dotenv';
 import {TelegramBotApi} from './lib';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
+const port = process.env.PORT;
 const token = process.env.BOT_TOKEN;
-
 const telegramBotApi = new TelegramBotApi(token);
 
-telegramBotApi.run();
+const app = express();
+
+app.post(token, (req, res) => {
+    console.log(req.body);
+
+    res.send('ok');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
+});
